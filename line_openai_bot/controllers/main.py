ff032090cb8ac12f6f65d60b7fcd13d1e5ad327a
87428467 <127670922+87428467@users.noreply.github.com>
2026-06-09 17:41:54 +0800

    Update main.py

--- line_openai_bot/controllers/main.py
+++ line_openai_bot/controllers/main.py
@@ -1,3 +1,9 @@
-line_openai_bot.channel_secret
-line_openai_bot.channel_access_token
-line_openai_bot.openai_api_key
+from odoo import http
+from odoo.http import request
+
+class LineBotController(http.Controller):
+    
+    @http.route('/line/webhook', type='http', auth='public', csrf=False)
+    def line_webhook(self, **kwargs):
+        # 這裡是未來接收 LINE 訊息的地方
+        return "OK"
